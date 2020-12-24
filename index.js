ddc6c28efcb06d21b36c693b5d864861b7368f16
Chris <chris.tisserat@gmail.com>
2020-12-24 08:44:30 -0800

    function working and both tests pass

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Calculate = {
-  factorial(number) {
-    return 120;
+  factorial(inputNumber) {
+    let factorialResult = inputNumber;
+    console.log(factorialResult)
+    for (let i = inputNumber - 1; i > 0; i--) {
+      factorialResult *= i;
+    };
+    return factorialResult; 
   }
 }
 
@@ -11,5 +16,9 @@ module.exports = Calculate;
 
 In your index.js file, add a factorial method inside the Calculate object
 The factorial method should accept one input and return 120
+*/
+
+/*6of11
+In your index.js file, replace the line of code return 120 with code that will actually calculate the factorial of the input argument passed to the .factorial() method
 */
 
